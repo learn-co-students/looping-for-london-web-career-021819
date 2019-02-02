88c5a4b365c5935ed15b25be82b7b6aa6edce7ec
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-02 23:36:07 +0000

    Done.

--- for.rb
+++ for.rb
@@ -1,7 +1,19 @@
 
 def using_for
 	checklist = 1..10
-	#your code here
+  for charm in checklist
+	puts "Wingardium Leviosa"
+ end
 end
 
+
+=begin
+def using_while
+  checklist = 0
+  while checklist < 10
+  puts "Wingardium Leviosa"
+  checklist += 1
+ end
+end
+=end
 
